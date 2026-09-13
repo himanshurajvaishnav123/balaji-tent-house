4507fe59dedb2639ea1b81366bc6d06e925198c5
himanshu <himanshurajvaishnav@gmail.com>
2026-09-13 15:32:15 +0530

    backend url updated

--- balaji-tent-house/client/src/api/client.ts
+++ balaji-tent-house/client/src/api/client.ts
@@ -7,7 +7,7 @@ import type {
   SiteSettings,
 } from "../types";
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+const API_BASE_URL = import.meta.env.VITE_API_URL || " https://balaji-tent-house-backend.onrender.com";
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
